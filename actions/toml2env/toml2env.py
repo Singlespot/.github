f7b1ba0b7e87202addf4178670f526dab1ea5d11
Sébastien de Melo <sebastien.demelo@protonmail.com>
2023-02-24 15:23:19 +0100

    first commit

---
+++ actions/toml2env/toml2env.py
@@ -0,0 +1,30 @@
+#!python
+import os
+import sys
+import tomllib
+from typing import Union
+
+
+def dict_to_env_vars(d: dict[str, Union[str, dict[str, any]]], prefix: str = '') -> dict[str, str]:
+    env_vars = {}
+    for key, value in d.items():
+        if isinstance(value, dict):
+            env_vars.update(dict_to_env_vars(value, prefix + key.upper() + '_'))
+        else:
+            env_vars[prefix + key.upper()] = value
+    return env_vars
+
+
+def main():
+    toml_file = sys.argv[1]
+    print(toml_file)
+    with open(toml_file, 'rb') as f:
+        content = tomllib.load(f)
+    print(content)
+    env_vars = dict_to_env_vars(content)
+    print(env_vars)
+    print(os.environ['GITHUB_ENV'])
+
+
+if __name__ == "__main__":
+    main()
