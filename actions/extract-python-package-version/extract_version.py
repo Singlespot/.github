e1e29f31d5ad320ac9bdf8700d39c6e242adc99c
Sébastien de Melo <sebastien.demelo@protonmail.com>
2025-02-27 15:50:26 +0100

    Added extract-python-package-version action

---
+++ actions/extract-python-package-version/extract_version.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+import tomllib
+from pathlib import Path
+
+
+def get_version(pyproject_path: Path) -> str | None:
+    try:
+        with pyproject_path.open("rb", encoding="utf-8") as f:
+            data = tomllib.load(f)
+
+        # Setuptools
+        if "project" in data and (version := data["project"].get("version")):
+            return version
+
+        # Poetry
+        if "tool" in data and "poetry" in data["tool"] and (version := data["tool"]["poetry"].get("version")):
+            return version
+
+    except (FileNotFoundError, tomllib.TOMLDecodeError):
+        pass
+
+    return None
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pyproject", default="pyproject.toml", help="Path to pyproject.toml")
+    args = parser.parse_args()
+
+    if pyversion := get_version(Path(args.pyproject)):
+        with open(os.getenv("GITHUB_ENV"), "a") as env_file:
+            env_file.write(f"VERSION={pyversion}\n")
+    else:
+        print("No version found in pyproject.toml")
+        exit(1)
