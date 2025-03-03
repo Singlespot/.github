0543bb49a46ad4438978b17e1be1b0c5e2b19384
Sébastien de Melo <sebastien.demelo@protonmail.com>
2025-03-03 15:27:34 +0100

    Updated action

--- actions/is-python-version-stable/check_version.py
+++ actions/is-python-version-stable/check_version.py
@@ -3,6 +3,11 @@ import os
 
 from packaging.version import parse, InvalidVersion
 
+
+def to_bool_str(value: bool) -> str:
+    return "true" if value else "false"
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("version")
@@ -16,7 +21,7 @@ if __name__ == "__main__":
     else:
         is_stable = not (version.is_devrelease or version.is_postrelease or version.is_prerelease)
         with open(os.environ["GITHUB_OUTPUT"], "a") as gh:
-            gh.write(f"stable={is_stable}\n")
-            gh.write(f"dev={version.is_devrelease}\n")
-            gh.write(f"post={version.is_postrelease}\n")
-            gh.write(f"pre={version.is_prerelease}\n")
+            gh.write(f"stable={to_bool_str(is_stable)}\n")
+            gh.write(f"dev={to_bool_str(version.is_devrelease)}\n")
+            gh.write(f"post={to_bool_str(version.is_postrelease)}\n")
+            gh.write(f"pre={to_bool_str(version.is_prerelease)}\n")
