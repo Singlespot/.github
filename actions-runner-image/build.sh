c51c0fe9425645e4ad43650378739bfd4631b14a
Sébastien de Melo <sebastien@singlespot.com>
2025-09-03 16:47:00 +0200

    Update actions-runner image

--- actions-runner-image/build.sh
+++ actions-runner-image/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-while getopts t: opts; do
+while getopts t:o: opts; do
    case ${opts} in
       t) IMAGE_TAG=${OPTARG} ;;
+      o) OUTPUT_TAG=${OPTARG} ;;
       *) echo "Unknown option: $opts" ;;
    esac
 done
@@ -12,10 +13,15 @@ if [ -z "${IMAGE_TAG}" ]; then
     exit 1
 fi
 
-echo "Building with IMAGE_TAG=${IMAGE_TAG}"
+if [ -z "${OUTPUT_TAG}" ]; then
+    OUTPUT_TAG=${IMAGE_TAG}
+    exit 1
+fi
+
+echo "Building with IMAGE_TAG=${IMAGE_TAG}, OUTPUT_TAG=${OUTPUT_TAG}"
 
 docker buildx build --push --builder kube --platform linux/amd64,linux/arm64 \
     --build-arg IMAGE_TAG="${IMAGE_TAG}" \
     --cache-from type=s3,region=eu-west-1,bucket=spt-docker-cache \
     --cache-to type=s3,region=eu-west-1,bucket=spt-docker-cache \
-		-t 268324876595.dkr.ecr.eu-west-1.amazonaws.com/spt-actions-runner:"${IMAGE_TAG}" "$(dirname "$0")"
+		-t 268324876595.dkr.ecr.eu-west-1.amazonaws.com/spt-actions-runner:"${OUTPUT_TAG}" "$(dirname "$0")"
